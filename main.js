9576ea88304c34f920fb8cbbff826b8965d1b25e
Daniel <daniel-litsesio@hotmail.com>
2017-02-07 13:33:37 +0100

    Fixar generering av bilder utifrån js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+//Metod för att randomisera placeringen av bilder
+
+var bilder = [
+    "<img class='1' id='a' src='bilder/g0000.gif'>",
+    "<img class='1' id='b' src='bilder/g0000.png'>",
+    "<img class='2' id='c' src='bilder/resample1.png'>",
+    "<img class='2' id='d' src='bilder/resample2.png'>",
+    "<img class='2' id='e' src='bilder/resample3bilineal.png'>",
+    "<img class='1' id='a' src='bilder/resampling4bicubic.png'>"
+];
+
+var container = document.getElementById("bilder");
+
+function printBilder() {
+    for(var i = 0; i < bilder.length; i++) {
+        container.innerHTML += bilder[i];
+    }
+};
+
+window.onload = printBilder;
+
+
+
+
+
+
 //inspiration till funktionen som gör att man kan klicka på alla bilder och skicka dess class
 //http://stackoverflow.com/questions/964119/how-to-get-the-class-of-the-clicked-element
 
