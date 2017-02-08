3e6838dcffe2724284d65b476e0f41cac20b8bde
Daniel <daniel-litsesio@hotmail.com>
2017-02-08 13:53:59 +0100

    hej

--- main.js
+++ main.js
@@ -10,7 +10,25 @@ var bilder = [
     "<img class='2' id='resample1' src='bilder/resample1.png'>",
     "<img class='2' id='resample2' src='bilder/resample2.png'>",
     "<img class='2' id='resample3' src='bilder/resample3bilineal.png'>",
-    "<img class='2' id='resample4' src='bilder/resampling4bicubic.png'>"
+    "<img class='2' id='resample4' src='bilder/resampling4bicubic.png'>",
+    "<img class='3' id='bitar1' src='bilder/2-bitar.png'>",
+    "<img class='3' id='bitar2' src='bilder/4-bitar.bmp'>",
+    "<img class='3' id='bitar3' src='bilder/8-bitar.bmp'>",
+    "<img class='3' id='bitar4' src='bilder/16-bitar.bmp'>",
+    "<img class='3' id='bitar5' src='bilder/24-bitar.bmp'>",
+    "<img class='4' id='filformat1' src='bilder/filformat.gif'>",
+    "<img class='4' id='filformat2' src='bilder/filformat.jpg'>",
+    "<img class='4' id='filformat3' src='bilder/filformat.png'>",
+    "<img class='5' id='antiAliasing1' src='bilder/antialiasing.jpg'>",
+    "<img class='5' id='antiAliasing2' src='bilder/jagged.jpg'>",
+    "<img class='6' id='falseColor1' src='bilder/false-color.jpg'>",
+    "<img class='6' id='falseColor2' src='bilder/true-color.jpg'>",
+    "<img class='7' id='effects1' src='bilder/jellyfish.jpg'>",
+    "<img class='7' id='effects2' src='bilder/jellyfishblur.jpg'>",
+    "<img class='7' id='effects3' src='bilder/jellyfishemboss.jpg'>",
+    "<img class='7' id='effects4' src='bilder/jellyfishsharp.jpg'>",
+    "<img class='8' id='moaire1' src='bilder/moaire.jpg'>",
+    "<img class='8' id='moaire2' src='bilder/moaire_fixed2.jpg'>"
 ];
 
 //hämtar elementet som bilderna ska läggas in i 
@@ -62,12 +80,29 @@ function resetKlicks() {
     klassKlick[1] = 0;
     klassKlick[2] = 0;
     klassKlick[3] = 0;
+    klassKlick[4] = 0;
+    
+    for(var i = 0; i < 5; i++) {
+        $("#" + idKlick[i]).css("border", "2px solid black");
+    }
+    
+    
     
     idKlick[0] = 0;
     idKlick[1] = 0;
     idKlick[2] = 0;
     idKlick[3] = 0;
+    idKlick[4] = 0;
+    
+    
 }
+
+$("#reset").click(function() {
+    resetKlicks();
+    alert("Dina klick är nu återställda!");
+});
+
+
 
 //Kollar så att alla klasser är samma
 function allSameClass() {
@@ -115,13 +150,19 @@ function toggleClass(className) {
 
 //När man klickar på en bild körs funktionen
 $("img").click(function() {
+        
+    $(this).css("border", "2px solid red");
     
     //kollar om det finns ett värde i variablerna och tilldelar klassens värde
+    
+    //Kollar vid försa klicket ------------------------
     if(klassKlick[0] == 0) {
         klassKlick[0] = $(this).attr("class"); 
         idKlick[0] = $(this).attr("id");
         alert(klassKlick[0]);
     } 
+    
+    //Kollar vid andra klicket ----------------------------------------
     else if(klassKlick[1] == 0) {
         klassKlick[1] = $(this).attr("class");
         idKlick[1] = $(this).attr("id");
@@ -137,13 +178,54 @@ $("img").click(function() {
             }           
             
         }
+        else if(klassKlick[0] == "5") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("5");
+                resetKlicks();
+            }           
+            
+        }
+        else if(klassKlick[0] == "6") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("6");
+                resetKlicks();
+            }           
+            
+        }
+          else if(klassKlick[0] == "8") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("8");
+                resetKlicks();
+            }           
+            
+        }
     }
+    
+    //Kollar vid tredje klicket -------------------------
     else if(klassKlick[2] == 0) {
         klassKlick[2] = $(this).attr("class");
         idKlick[2] = $(this).attr("id");
         alert(klassKlick[2]);
+        
+        if(klassKlick[0] == "4") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("4");
+                resetKlicks();
+            }           
+            
+        }
 
     }
+    
+    //Kollar vid fjärde klicket ----------------------------------
     else if(klassKlick[3] == 0) {
         klassKlick[3] = $(this).attr("class");
         idKlick[3] = $(this).attr("id");
@@ -157,15 +239,43 @@ $("img").click(function() {
                 toggleClass("2");
                 resetKlicks();
                 
-            }     
-            else {
-                resetKlicks();
-                alert("Du hittade inte alla par...");
-            }
+            } 
+            
         }
+            
+        else if(klassKlick[0] == "7") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("7");
+                resetKlicks();
+            }           
+            
+        }
+    }
+    else if(klassKlick[4] == 0) {
+        klassKlick[4] = $(this).attr("class"); 
+        idKlick[4] = $(this).attr("id");
+        alert(klassKlick[0]);
+        
+        if(klassKlick[0] == "3") {
+            
+            //Om alla klick har samma klass men olika ID är paret godkänt
+            if(allSameClass() == true && differentId() == true) {
+                toggleClass("3");
+                resetKlicks();
+            }           
+            
+        }
+        
         else {
             resetKlicks();
             alert("Du hittade inte alla par...");
         }
-    }
+    } 
+    
+    
+    
+    
+            
 });
